25ae55dec95ea679c32691673a7963c3580a1659
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 15:10:55 +0300

    Q13

--- Program.cs
+++ Program.cs
@@ -1,34 +1,34 @@
-﻿using System.Runtime.Intrinsics.X86;
-using System.Text;
-using System.Threading.Channels;
+﻿//using System.Runtime.Intrinsics.X86;
+//using System.Text;
+//using System.Threading.Channels;
 
-namespace c_assignment1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace c_assignment1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //Q13   
-            {
+//            //Q13   
+//            {
 
-                Book? book = null;
-                if (book != null)
-                {
-                    Console.WriteLine("null");
-                }
-                else
-                {
-                    Console.WriteLine("not null");
-                }
-            }
-        }
-    }
+//                Book? book = null;
+//                if (book != null)
+//                {
+//                    Console.WriteLine("null");
+//                }
+//                else
+//                {
+//                    Console.WriteLine("not null");
+//                }
+//            }
+//        }
+//    }
 
 
-    class Book
-    {
-        string title;
-        int pages;
-    }
-}
+//    class Book
+//    {
+//        string title;
+//        int pages;
+//    }
+//}
