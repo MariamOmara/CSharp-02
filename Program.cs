51c7aaf095c7364e0a9842ee0bdc99b2f0c900bd
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 14:45:51 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
-﻿using System.Runtime.Intrinsics.X86;
-using System.Text;
-using System.Threading.Channels;
+﻿//using System.Runtime.Intrinsics.X86;
+//using System.Text;
+//using System.Threading.Channels;
 
-namespace c_assignment1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //Q1
-            {
-                Book B1 = new Book();
-                object name = B1;
-                Console.WriteLine(name);
-            }
-        }
+//namespace c_assignment1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Q1
+//            {
+//                Book B1 = new Book();
+//                object name = B1;
+//                Console.WriteLine(name);
+//            }
+//        }
 
 
      
-        class Book
-        {
-            string title;
-            int pages;
-        }
-    }
-}
+//        class Book
+//        {
+//            string title;
+//            int pages;
+//        }
+//    }
+//}
