bb544d6d62e97d04aa3b7f2aac208900089cd194
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 15:12:10 +0300

    Q14

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿//using System.Runtime.Intrinsics.X86;
-//using System.Text;
-//using System.Threading.Channels;
-
+﻿
 //namespace c_assignment1
 //{
 //    internal class Program
@@ -9,26 +6,21 @@
 //        static void Main(string[] args)
 //        {
 
-//            //Q13   
+//            // Q14
+
 //            {
 
-//                Book? book = null;
-//                if (book != null)
-//                {
-//                    Console.WriteLine("null");
-//                }
-//                else
-//                {
-//                    Console.WriteLine("not null");
-//                }
+//                string? title = null;
+
+//                Console.WriteLine(title ?? "Untitled");
+
+//                title ??= "Untitled";
+
+//                Console.WriteLine(title);
+
 //            }
 //        }
 //    }
-
-
-//    class Book
-//    {
-//        string title;
-//        int pages;
-//    }
 //}
+
+    
