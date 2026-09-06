4e763de1098c3cfda2780f251ec439cb9b914087
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 14:43:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Channels;
+
+namespace c_assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            {
+                Book B1 = new Book();
+                object name = B1;
+                Console.WriteLine(name);
+            }
+        }
+
+
+     
+        class Book
+        {
+            string title;
+            int pages;
+        }
+    }
+}
